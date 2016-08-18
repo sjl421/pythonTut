322e92be1eb930efe993272d21387c3d25efca39
kongming <kongming@jd.com>
2016-08-18 19:38:08 +0800

    tt Signed-off-by: kongming <kongming@jd.com>

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print 'hello python'
+print '100+200=',100+200
+name=raw_input('please enter your name:')
+print 'hello ',name
+
+print '''line1
+line2
+line3'''
---
+++ tut1.py
@@ -0,0 +1,5 @@
+a='ABC'
+b=a
+a='xyz'
+print b
+print a
